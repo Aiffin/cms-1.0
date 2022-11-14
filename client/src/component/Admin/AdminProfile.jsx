36c462aa3b99aa11ca265e2b2c5d697289b2ac70
Aiffin <aiffinsk@gmail.com>
2022-11-14 16:37:28 +0530

    student dashboard updated

--- client/src/component/Admin/AdminProfile.jsx
+++ client/src/component/Admin/AdminProfile.jsx
@@ -1,8 +1,154 @@
-import React from 'react'
+import React,{useContext,useState} from 'react'
+import { DataContext } from '../../GlobalContext'
+import axios from 'axios'
+import {toast} from 'react-toastify'
+
+const Loading = () =>{
+  return <div className="spinner-border text-success" role={'status'}>
+    <span className="visually-hidden">Loading...</span>
+
+  </div>
+}
 
 function AdminProfile() {
+  const context = useContext(DataContext)
+  const token= context.token
+  const [currentUser] = context.data.authApi.currentUser
+  const [img,setImg] = useState(false)
+  const [loading,setLoading]=useState(false)
+
+  const handleUpload = async(e) => {
+    e.preventDefault();
+    try {
+      const file = e.target.files[0];
+      //console.log('img file=',file)
+      if(!file)
+      return toast.error('file not exists.. choose image to upload')
+
+//size validation
+      if(file.size > 1*1024*1024)
+      return toast.warning("File size must be below 1 mb")
+
+      //append in form contructor
+      let formData =new FormData()
+      formData.append('profileImg',file)
+      setLoading(true)
+
+      //post the img to server
+
+      const res = await axios.post(`/api/v1/image/profileImage/upload`,formData,{
+        headers:{
+          "Content-Type":"multipart/form-data",
+          Authorization:token
+        }
+      })
+
+      //update db file
+      await axios.patch(`/api/v1/user/update`,{
+        headers:{
+          Authorization:token
+        }
+      })
+ 
+      //after upload
+      setLoading(false);
+      setImg(res.data);
+
+    } catch (err) {
+      toast.error(err.response.data.msg)
+    }
+  }
+
+  const handleDestroy=async(e)=>{
+    try {
+      if(window.confirm(`Are you sure to delete profile Imagee?`)){
+        setLoading(true)
+        await axios.post(`/api/v1/image/profileImage/delete`,{public_id: img.public_id},{
+          headers:{
+            Authorization:token
+          }
+        })
+        setImg(false)
+        setLoading(false)
+        window.location.href="/admin/profile"
+      }
+    } catch (err) {
+      toast.error(err.response.data.msg)
+    }
+  }
+
+  const submitHandler = async(e) => {
+
+  }
   return (
-    <div>AdminProfile</div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h3 className="display-3">AdminProfile</h3>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card">
+            <div className="row">
+
+            <div className="col-md-4">
+             <div className="card border-0">
+              <div className="position-relative">
+                {
+                  img ? (<button onClick={handleDestroy} className='className="position-absolute top-0 start-100 bg-danger border border-light rounded-circle translate-middle pt-0 ps-2 pe-2 text-white'>x</button>):null
+                }
+                {
+                   img ? <img src={img ? img.url :""} alt="No image found" className='card-img' /> : 
+                   <img src={currentUser.image ? currentUser.image.url :""} alt="No image found" className='card-img' />
+                }
+             
+              {
+                loading ? <Loading/> : null
+              }
+              </div>
+             <div className="card-footer">
+             {
+                img ? (
+                  <div className="form-group text-center">
+                    <button className="btn btn-success">Save Profile</button>
+                  </div>
+                ) :(
+                  <div className="form-group">
+                    <input type="file" name={"profileImg"} id={"profileImg"} onChange={handleUpload} className="form-control"/>
+                  </div>
+                )
+              }
+             </div>
+             </div>
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h4 className="card-title text-center text-uppercase text-success">{currentUser.name}</h4>
+              </div>
+              <hr />
+              <p className="card-text">
+                <strong>Email</strong>
+                <strong className="float-end text-danger">{currentUser.email}</strong>
+              </p>
+              <hr />
+              <p className="card-text">
+                <strong>Mobile</strong>
+                <strong className="float-end text-danger">{currentUser.mobile}</strong>
+              </p>
+              <hr />  <p className="card-text">
+                <strong>Role</strong>
+                <strong className="float-end text-danger">{currentUser.role}</strong>
+              </p>
+        
+
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
